bf4854e9c767bde0ed4703f168455717d39a4486
Olevia Yuen <aivelo@icloud.com>
2022-09-02 16:18:40 +0800

    asd

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ImageBackground, StyleSheet, View, Text} from 'react-native';
+import CoverPage from './src/CoverPage';
 
-const HelloWorldApp = () => {
+const image = require('MyApp/images/cover-page-bg.jpeg');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bgImg: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+  centerBox: {
+    width: '70%',
+    height: '60%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+  },
+});
+
+const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.bgImg}>
+        <View style={styles.centerBox}>
+          <CoverPage />
+        </View>
+      </ImageBackground>
     </View>
   );
 };
-
-export default HelloWorldApp;
+export default App;
