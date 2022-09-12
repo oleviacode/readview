2ab1c7371cf37ae32ea5f7a12ac6b8f317dbbc8a
Olevia Yuen <aivelo@icloud.com>
2022-09-12 15:55:23 +0800

    scroll view added

--- MyApp/src/screens/main/Main.tsx
+++ MyApp/src/screens/main/Main.tsx
@@ -1,12 +1,11 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {View, Text, Pressable, StyleSheet, ScrollView} from 'react-native';
 import {logOut} from '../../../redux/auth/action';
 import {useAppDispatch} from '../../../redux/store';
 import {NaviProps} from '../../model';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {Button} from '@rneui/themed';
 import {HStack} from '@react-native-material/core';
+import {Button} from '@rneui/themed';
 
 import Chat from '../chat/Chat';
 
@@ -14,30 +13,78 @@ export default function MainScreen({navigation}: NaviProps) {
   const dispatch = useAppDispatch();
 
   return (
-    <View>
-      <Text>hello</Text>
-      <HStack m={4} spacing={6}>
-        <Button>Primary</Button>
-        <Button style={{backgroundColor: 'pink'}}>Secondary</Button>
-        <Button color="green">Warning</Button>
-        <Button color="red">Error</Button>
-      </HStack>
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.latestBookSection}>
+          <Text style={styles.titleText}>Latest Books</Text>
+          <HStack style={styles.bookStack}>
+            <View style={styles.book} />
+            <View style={styles.book} />
+            <View style={styles.book} />
+          </HStack>
+        </View>
 
-      <Text> I am Homepage Yah</Text>
-      <Pressable>
-        <Text
-          onPress={() => {
-            dispatch(logOut());
-            navigation.navigate('Cover');
-          }}>
-          Logout
-        </Text>
-      </Pressable>
+        <View style={styles.rankingSection}>
+          <Text style={styles.titleText}>Ranking</Text>
+          <ScrollView horizontal={true} showsVerticalScrollIndicator={false}>
+            <HStack>
+              <View style={styles.rankBox} />
+              <View style={styles.rankBox} />
+              <View style={styles.rankBox} />
+            </HStack>
+          </ScrollView>
+        </View>
+
+        <Pressable>
+          <Text
+            onPress={() => {
+              dispatch(logOut());
+              navigation.navigate('Cover');
+            }}>
+            Logout
+          </Text>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  topBar: {},
+  container: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  latestBookSection: {
+    width: '100%',
+    height: 160,
+    backgroundColor: 'pink',
+    marginTop: 10,
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  bookStack: {
+    flex: 1,
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  book: {
+    width: 100,
+    height: 110,
+    backgroundColor: 'grey',
+  },
+  rankingSection: {
+    height: 250,
+    backgroundColor: 'pink',
+    marginTop: 10,
+  },
+  rankBox: {
+    width: 200,
+    height: 210,
+    backgroundColor: 'grey',
+    margin: 10,
+    borderRadius: 10,
+  },
 });
--- MyApp/src/screens/search/Search.tsx
+++ MyApp/src/screens/search/Search.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import {Text} from 'react-native';
+import {Button} from '@rneui/themed';
+import {HStack} from '@react-native-material/core';
 
 export default function Search() {
-  return <Text>Search</Text>;
+  return (
+    <HStack spacing={6}>
+      <Button>Primary</Button>
+      <Button style={{backgroundColor: 'pink'}}>Secondary</Button>
+      <Button color="green">Warning</Button>
+      <Button color="red">Error</Button>
+    </HStack>
+  );
 }
